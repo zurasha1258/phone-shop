d4ce6dbd0b592a30af1e3ce0de0f8f52ff45e90c
zurasha1258 <zchaduneli72@gmail.com>
2025-01-05 23:15:05 +0400

    remove dublicates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 from configs import app
 from routes import add_product, login, logout, self, register, delete_phone, search, buy, product_detail, index
-=======
-from configs import app
-from routes import add_product, login, logout, self, register, delete_phone, search, buy, product_detail, index
->>>>>>> 1b01b77 (gitignoe added)
+
