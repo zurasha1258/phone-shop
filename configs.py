126132873abc282e8c6b11e05983bb164ace9f19
zurasha1258 <zchaduneli72@gmail.com>
2025-01-05 23:00:26 +0400

    fix dublicates

--- configs.py
+++ configs.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -12,18 +11,3 @@ class Configs:
 app = Flask(__name__)
 app.config.from_object(Configs)
 db = SQLAlchemy(app)
-=======
-import os
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-class Configs:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'uploads')
-
-app = Flask(__name__)
-app.config.from_object(Configs)
-db = SQLAlchemy(app)
->>>>>>> 1b01b77 (gitignoe added)
--- forms.py
+++ forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField, FileField
 from wtforms.validators import DataRequired
@@ -24,30 +23,3 @@ class LoginForm(FlaskForm):
 class SearchForm(FlaskForm):
     query = StringField('Search', validators=[DataRequired()])
     submit = SubmitField('Search')
-=======
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, FileField
-from wtforms.validators import DataRequired
-
-class AddProductForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    price = IntegerField('Price', validators=[DataRequired()])
-    img = FileField('Product Image', validators=[DataRequired()])
-    submit = SubmitField('Add Product')
-
-class UserForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    phone = StringField('Phone', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Register')
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
-
-class SearchForm(FlaskForm):
-    query = StringField('Search', validators=[DataRequired()])
-    submit = SubmitField('Search')
->>>>>>> 1b01b77 (gitignoe added)
