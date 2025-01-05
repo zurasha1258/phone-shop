f1c61e22c814c245e22f27d701d5d4dddd950f91
zurasha1258 <zchaduneli72@gmail.com>
2025-01-05 22:47:56 +0400

    Resolved merge conflict in app.py

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
-<<<<<<< HEAD
 from configs import app, db
 from flask_login import LoginManager
 from models import User
 from routes import *
 
-
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-
-
-=======
-from configs import app, db
-from flask_login import LoginManager
-from models import User
-from routes import *
-
-
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
-
-
->>>>>>> 1b01b77 (gitignoe added)
